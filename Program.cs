ca1392dc50422986e22522c1b7c9c47eac4ac07c
phum <aekkaratthaphum@outlook.co.th>
2023-05-15 16:09:02 +0700

    1

--- Program.cs
+++ Program.cs
@@ -2,6 +2,77 @@
 
 class Program{
     static void Main(string[] args){
+
+       Exam1Water();
+
+    }
+
+    static void Exam1Water(){
+         Console.WriteLine("Enter Vmax : ");
+        float vMax = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Vdrink : ");
+        float vDrink = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Vfill : ");
+        float vFill = float.Parse(Console.ReadLine());
+
+ 
+        Console.WriteLine("Enter TDrink : ");
+        float tDrink = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Tfill : ");
+        float tFill = float.Parse(Console.ReadLine());
+
+        Console.WriteLine("Enter Tday : ");
+        float tDay = float.Parse(Console.ReadLine());
+
+        
+        float sumDrink = 0;
+        float sumFill = 0;
+        float vCurrent = vMax;
+        float countTFIll = tFill ;
+        float countTDrink = tDrink;
+        float hourCount = 0;
+
+         while(true){
+
+                if(vCurrent <= 0 ){
+                    Console.WriteLine("Not Enough Water");
+                    break;
+                }
+
+                if(hourCount > tDay){
+                    break;
+                }
+
+               
+                 if(hourCount%tDrink == 0){
+                    Console.WriteLine("Drink");
+                       vCurrent -= vDrink;
+                 }
+                 if(hourCount%tFill == 0){
+                    Console.WriteLine("Fill");
+                    vCurrent += tFill;
+                    if(vCurrent > vMax){
+                          vCurrent = vMax;
+                    }
+                }
+                //Console.WriteLine("count Drink : "+countTDrink);
+                //Console.WriteLine("count Fill : "+countTFIll);
+                Console.WriteLine("Hour : " + hourCount);
+                Console.WriteLine("VCurrent : "+vCurrent);
+                countTDrink += tDrink;
+                countTFIll += tFill;
+                hourCount++;
+          
+         
+         }
+
+   
+  
+
+
         
     }
 }
